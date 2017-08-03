dea32bc0ac4ac133480079ff1c9c979289509306
danielkalen <d@danielkalen.com>
2017-08-03 13:45:05 -0400

    [Add-on] avoid using highlight.js for bash code

--- src/components/test/code-snippet.jsx
+++ src/components/test/code-snippet.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash/isEqual';
 import hljs from 'highlight.js/lib/highlight';
+import Ansi from 'ansi-to-html';
 import classNames from 'classnames/bind';
 import styles from './test.css';
 
+const ansi = new Ansi();
 const cx = classNames.bind(styles);
 
 class CodeSnippet extends Component {
@@ -27,7 +29,9 @@ class CodeSnippet extends Component {
   };
 
   componentDidMount() {
-    this.highlightCode();
+    if (this.props.lang !== 'bash') {
+      this.highlightCode();
+    }
   }
 
   shouldComponentUpdate(nextProps) {
@@ -50,6 +54,8 @@ class CodeSnippet extends Component {
       const expected = `<span class='${cx('code-diff-expected')}'>+ expected</span>`;
       const actual = `<span class='${cx('code-diff-actual')}'>- actual</span>`;
       codeHtml = `${expected}&nbsp;&nbsp;${actual}\n\n${code}`;
+    } else if (lang === 'bash') {
+      codeHtml = ansi.toHtml(code || '');
     }
 
     const cxName = cx(className, lang, { hljs: !highlight });
